c08d2c029b97b6cae6c777c2f33c726501846fce
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-05-10 21:01:46 +0000

    added edit action

--- app/controllers/wine_controller.rb
+++ app/controllers/wine_controller.rb
@@ -1,11 +1,67 @@
 class WineController < ApplicationController
 
-get '/wines' do
-  if is_logged_in?
-  erb :'wines/wines'
-else
-  redirect '/login'
-end
-end
+  get '/wines' do
+    if is_logged_in?
+      erb :'wines/wines'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/wines/new' do
+    if  is_logged_in?
+      erb :'wines/create_wine'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/wines/:id' do
+    if  !is_logged_in?
+      redirect '/login'
+    else
+      @user = current_user
+      @wine = Wine.find_by_id(params[:id])
+      erb :'/wines/show_wine'
+    end
+  end
+
+  post '/wines' do
+    is_logged_in?
+    if params[:name].empty?
+      redirect '/wines/new'
+    else @wine = Wine.create(params)
+    end
+    current_user.wines << @wine
+
+    redirect "/users/#{current_user.id}"
+  end
+
+  get '/wines/:id/edit' do
+    if  !is_logged_in?
+      redirect '/login'
+    else
+      @wine = Wine.find_by_id(params[:id])
+      erb :'/wines/edit_wine'
+    end
+  end
+
+  patch '/wines/:id' do
+  if
+    !is_logged_in?
+      redirect '/login'
+
+  else
+    @wine = Wine.find(params[:id])
+      if @wine.name.empty?
+      redirect "/wines/#{@tweet.id}/edit"
+    end
+    @wine.update(name: params[:name])
+    @wine.save
+    redirect "/wines/#{@wine.id}"
+
+  end
+  end
+
 
 end
